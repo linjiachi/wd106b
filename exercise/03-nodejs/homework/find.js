058f3ae1cfd8b4bff8505d9bd001c4719ce83fa5
linjiachi <smile01723@gmail.com>
2018-03-31 11:45:12 +0800

    add homework

--- exercise/03-nodejs/homework/find.js
+++ exercise/03-nodejs/homework/find.js
@@ -1,14 +1,16 @@
-//4-2
+//4-2陣列處理
 function find(array, item){
     for(let i=0; i<array.length; i++){
-        if (array[i] === item)/*三個等於是型態一樣*/
-        return i
+        if (array[i] === item)/*三個等於是型態,內容一樣*/
+        return i              /*兩個等於是內容一樣*/
     }
     return -1
 }
 
+/*變數宣告: var前後皆可使用 let只有之後的程式碼或一整個區塊{}*/
+
 let idx = find([ 1, 4, 5, 8, 9], 5)
-console.log('idx=', idx)
+console.log('idx=', idx)//=>idx= 2
 
 idx = find([ 1, 4, 5, 8, 9], 6)
-console.log('idx=', idx)
+console.log('idx=', idx)//=>idx= -1
