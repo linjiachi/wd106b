d85ef63321a4adfe43f43461e837e1647c08168a
linjiachi <smile01723@gmail.com>
2018-03-31 10:00:11 +0800

    add 03-node.js

---
+++ exercise/03-nodejs/homework/factorial.js
@@ -0,0 +1,9 @@
+function factorial(n){
+    var result = 1;
+    for(var i=1;i<=n;i++){
+        result = result * i
+    }
+    return result
+}
+console.log('factorial(5)=', factorial(5))
+console.log('factorial(10)=', factorial(10))
---
+++ exercise/03-nodejs/homework/sqrt.js
@@ -0,0 +1,8 @@
+function sqrt(n){
+    let x = 0
+    for (x=0; x*x < n; x+=0.001){
+    }/*代表程式碼正式結束*/
+    return x
+}
+
+console.log('sqrt(4)=', sqrt(4))
